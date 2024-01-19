6d613365f8c2b4b20e4d7fb25afa76ab88dd7930
nurulabsr <absar.ru@yandex.com>
2024-01-19 22:36:46 +0600

    updated auth

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('admin/dashboard', [AdminController::class, 'Dashboard'])->middleware()->name('admin.dashboard');
+Route::get('admin/dashboard', [AdminController::class, 'Dashboard'])->middleware('auth')->name('admin.dashboard');
 Route::get('vendor/dashboard', [VendorController::class, 'Dashboard'])->middleware('auth')->name('vendor.dashboard');
 
 
