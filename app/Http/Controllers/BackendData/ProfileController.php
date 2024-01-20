e8aac05a91733fa281a81ff5256a65e56d1f421f
nurulabsr <absar.ru@yandex.com>
2024-01-20 18:29:21 +0600

    added profile controller and index.blade.php of profile

--- app/Http/Controllers/BackendData/ProfileController.php
+++ app/Http/Controllers/BackendData/ProfileController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function index(){
+    public function Index(){
         return view('admin.profile.index');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\BackendData\AdminController;
+use App\Http\Controllers\BackendData\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //admin dashboard
 Route::get('dashboard', [AdminController::class, 'Dashboard'])->name('dashboard');  //->middleware(['auth', 'role:admin']) in the RouteServiceProvider.php, name('admin.dashboard'); is change via as(admin.), admin/dashboard via prefix()
+Route::get('profile', [ProfileController::class, 'Index'])->name('profile');
