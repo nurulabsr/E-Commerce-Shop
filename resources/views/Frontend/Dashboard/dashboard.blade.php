c6bc1fb68a2627555cc20641be6990fc083b8706
nurulabsr <absar.ru@yandex.com>
2024-01-21 21:20:39 +0600

    working with dashboard....

--- resources/views/Frontend/Dashboard/dashboard.blade.php
+++ resources/views/Frontend/Dashboard/dashboard.blade.php
@@ -1,31 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-  <title>One Shop || e-Commerce HTML Template</title>
-  <link rel="icon" type="image/png" href="images/favicon.png">
-  <link rel="stylesheet" href="css/all.min.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/select2.min.css">
-  <link rel="stylesheet" href="css/slick.css">
-  <link rel="stylesheet" href="css/jquery.nice-number.min.css">
-  <link rel="stylesheet" href="css/jquery.calendar.css">
-  <link rel="stylesheet" href="css/add_row_custon.css">
-  <link rel="stylesheet" href="css/mobile_menu.css">
-  <link rel="stylesheet" href="css/jquery.exzoom.css">
-  <link rel="stylesheet" href="css/multiple-image-video.css">
-  <link rel="stylesheet" href="css/ranger_style.css">
-  <link rel="stylesheet" href="css/jquery.classycountdown.css">
-  <link rel="stylesheet" href="css/venobox.min.css">
-
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/responsive.css">
-  <!-- <link rel="stylesheet" href="css/rtl.css"> -->
-</head>
+@include('Frontend.Common.header')
 
 <body>
 
@@ -227,43 +203,7 @@
   ==============================-->
 
 
-  <!--jquery library js-->
-  <script src="js/jquery-3.6.0.min.js"></script>
-  <!--bootstrap js-->
-  <script src="js/bootstrap.bundle.min.js"></script>
-  <!--font-awesome js-->
-  <script src="js/Font-Awesome.js"></script>
-  <!--select2 js-->
-  <script src="js/select2.min.js"></script>
-  <!--slick slider js-->
-  <script src="js/slick.min.js"></script>
-  <!--simplyCountdown js-->
-  <script src="js/simplyCountdown.js"></script>
-  <!--product zoomer js-->
-  <script src="js/jquery.exzoom.js"></script>
-  <!--nice-number js-->
-  <script src="js/jquery.nice-number.min.js"></script>
-  <!--counter js-->
-  <script src="js/jquery.waypoints.min.js"></script>
-  <script src="js/jquery.countup.min.js"></script>
-  <!--add row js-->
-  <script src="js/add_row_custon.js"></script>
-  <!--multiple-image-video js-->
-  <script src="js/multiple-image-video.js"></script>
-  <!--sticky sidebar js-->
-  <script src="js/sticky_sidebar.js"></script>
-  <!--price ranger js-->
-  <script src="js/ranger_jquery-ui.min.js"></script>
-  <script src="js/ranger_slider.js"></script>
-  <!--isotope js-->
-  <script src="js/isotope.pkgd.min.js"></script>
-  <!--venobox js-->
-  <script src="js/venobox.min.js"></script>
-  <!--classycountdown js-->
-  <script src="js/jquery.classycountdown.js"></script>
-
-  <!--main/custom js-->
-  <script src="js/main.js"></script>
+@include('Frontend.Common.footer')
 </body>
 
 </html>
