f24502182384989a28ae44bfff43811324df972d
nurulabsr <absar.ru@yandex.com>
2024-01-23 01:21:10 +0600

    Working with Slider from admin dashboard

--- resources/views/vendor/Dashboard/Layouts/sidebar.blade.php
+++ resources/views/vendor/Dashboard/Layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     </span>
     <a href="{{route('home')}}" class="dash_logo"><img src="{{asset('FrontendData/images/logo.png')}}" alt="logo" class="img-fluid"></a>
     <ul class="dashboard_link">
-      <li><a class="active" href="{{route('user.dashboard')}}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
+      <li><a class="active" href="{{route('vendor.dashboard')}}"><i class="fas fa-tachometer"></i>Dashboard</a></li>
       <li><a href=""><i class="fas fa-list-ul"></i> Orders</a></li>
       <li><a href="dsahboard_download.html"><i class="far fa-cloud-download-alt"></i> Downloads</a></li>
       <li><a href="dsahboard_review.html"><i class="far fa-star"></i> Reviews</a></li>
