074ace18b813be8273a6086b8aac19da11018136
nurulabsr <absar.ru@yandex.com>
2024-01-23 01:32:07 +0600

    Working with Slider from admin dashboard

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,34 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>General Dashboard &mdash; Stisla</title>
-
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/bootstrap/css/bootstrap.min.css')}}">
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/fontawesome/css/all.min.css')}}">
-
-  <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/jqvmap/dist/jqvmap.min.css')}}">
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/weather-icon/css/weather-icons.min.css')}}">
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/weather-icon/css/weather-icons-wind.min.css')}}">
-  <link rel="stylesheet" href="{{asset('BackendData/assets/modules/summernote/summernote-bs4.css')}}">
-
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="{{asset('BackendData/assets/css/style.css')}}">
-  <link rel="stylesheet" href="{{asset('BackendData/assets/css/components.css')}}">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
-
+@include('admin.CommonLink.header')
 <body>
   <div id="app">
     <div class="main-wrapper main-wrapper-1">
@@ -52,27 +24,6 @@
   </div>
 
   <!-- General JS Scripts -->
-  <script src="{{asset('BackendData/assets/modules/jquery.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/popper.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/tooltip.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/moment.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/js/stisla.js')}}"></script>
-  
-  <!-- JS Libraies -->
-  <script src="{{asset('BackendData/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/chart.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/summernote/summernote-bs4.js')}}"></script>
-  <script src="{{asset('BackendData/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
-
-  <!-- Page Specific JS File -->
-  <script src="{{asset('BackendData/assets/js/page/index-0.js')}}"></script>
- 
-  <!-- Template JS File -->
-  <script src="{{asset('BackendData/assets/js/scripts.js')}}"></script>
-  <script src="{{asset('BackendData/assets/js/custom.js')}}"></script>
+   @include('admin.CommonLink.footer')
 </body>
 </html>
