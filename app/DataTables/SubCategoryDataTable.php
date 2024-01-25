6a5f8d22c304675af184dc22bec6430703d24026
nurulabsr <absar.ru@yandex.com>
2024-01-26 03:08:30 +0600

    SubCategory Yajra Data table

--- app/DataTables/SubCategoryDataTable.php
+++ app/DataTables/SubCategoryDataTable.php
@@ -22,10 +22,30 @@ class SubCategoryDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'subcategory.action')
-            ->addColumn('category', function($query){
-                 
+            ->addColumn('action', function($query){
+                $editBtn = '<a href="'.route("admin.sub-category.edit", $query->id).'" class="btn btn-primary btn-sm"><i class="fa-regular fa-pen-to-square"></i>Edit</a>';
+                $deleteBtn='<a href="'.route("admin.sub-category.destroy", $query->id).'" class="btn btn-warning btn-sm ml-2 delete-item"><i class="fa-solid fa-trash"></i>Delete</a>';
+                return $editBtn.$deleteBtn;
             })
+            ->addColumn('category', function($query){
+                 return $query->category->category_name;
+            })
+            ->addColumn('sub_category_status', function($query){
+                if($query->sub_category_status==1){
+                   $toggleBtn = '<label>
+                   <input type="checkbox" checked name="custom-switch-checkbox" class="custom-switch-input status" data-id="'.$query->id.'">
+                   <span class="custom-switch-indicator"> </span>
+                  </label>';
+               return $toggleBtn;
+                } else{
+                   $toggleBtn = '<label>
+                   <input type="checkbox" name="custom-switch-checkbox" class="custom-switch-input status" data-id="'.$query->id.'">
+                   <span class="custom-switch-indicator"> </span>
+                 </label>';
+                 return $toggleBtn;
+                }
+           })
+           ->rawColumns(['sub_category_status', 'action'])
             ->setRowId('id');
     }
 
@@ -47,7 +67,7 @@ class SubCategoryDataTable extends DataTable
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
-                    ->orderBy(1)
+                    ->orderBy(0)
                     ->selectStyleSingle()
                     ->buttons([
                         Button::make('excel'),
@@ -66,13 +86,15 @@ class SubCategoryDataTable extends DataTable
     {
         return [
             Column::make('id'),
-            Column::make('add your columns'),
+            Column::make('sub_category_name'),
+            Column::make('category')->title('Category Name'),
+            Column::make('sub_category_status')->title('Sub Category Status'),
+            Column::make('sub_category_slug')->title('Slug'),
             Column::make('created_at'),
-            Column::make('updated_at'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(160)
                   ->addClass('text-center'),
             
         ];
