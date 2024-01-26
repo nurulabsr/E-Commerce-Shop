ee8a62e199fa10d5a5d1219e3ba418d092f11f40
nurulabsr <absar.ru@yandex.com>
2024-01-26 19:12:36 +0600

    child_categories

--- database/migrations/2024_01_25_231120_create_child_categories_table.php
+++ database/migrations/2024_01_25_231120_create_child_categories_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('child_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('child_category_name');
+            $table->string('child_category_slug');
+            $table->boolean('child_category_status');
+            $table->integer('category_id');
+            $table->integer('sub_category_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -22,6 +28,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('child_categories', function(Blueprint $table){
+             $table->dropSoftDeletes();
+        });
         Schema::dropIfExists('child_categories');
     }
 };
