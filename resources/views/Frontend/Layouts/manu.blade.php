336ad894a5b1980302ccf38e74479b890b42db2c
nurulabsr <absar.ru@yandex.com>
2024-01-31 21:20:43 +0600

    working with mobile menu same to above

--- resources/views/Frontend/Layouts/manu.blade.php
+++ resources/views/Frontend/Layouts/manu.blade.php
@@ -1,5 +1,12 @@
 @php
-    $categories = \App\Models\Category::all();
+    $categories = \App\Models\Category::where('category_status', 1)
+    ->with(['sub_category' => function($query){
+        $query->where('sub_category_status', 1)
+        ->with(['childCategory' => function($query){
+            $query->where('child_category_status', 1);
+        }]);
+    }])
+    ->get();
 @endphp
 <nav class="wsus__main_menu d-none d-lg-block">
     <div class="container">
@@ -179,74 +186,23 @@
                 <div class="wsus__mobile_menu_main_menu">
                     <div class="accordion accordion-flush" id="accordionFlushExample">
                         <ul class="wsus_mobile_menu_category">
-                            <li><a href="#"><i class="fas fa-star"></i> hot promotions</a></li>
+                            {{-- <li><a href="#"><i class="fas fa-star"></i> hot promotions</a></li> --}}
+                            @foreach ($categories as $category)
                             <li><a href="#" class="accordion-button collapsed" data-bs-toggle="collapse"
                                     data-bs-target="#flush-collapseThreew" aria-expanded="false"
-                                    aria-controls="flush-collapseThreew"><i class="fal fa-tshirt"></i> fashion</a>
+                                    aria-controls="flush-collapseThreew"><i class="{{$category->category_icon}}"></i> {{$category->category_name}}</a>
                                 <div id="flush-collapseThreew" class="accordion-collapse collapse"
                                     data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
+                                        @foreach ($category->sub_category as $subCategory)
                                         <ul>
-                                            <li><a href="#">men's</a></li>
-                                            <li><a href="#">wemen's</a></li>
-                                            <li><a href="#">kid's</a></li>
-                                            <li><a href="#">others</a></li>
+                                            <li><a href="#">{{$subCategory->sub_category_name}}</a></li>
                                         </ul>
+                                        @endforeach
                                     </div>
                                 </div>
                             </li>
-                            <li><a href="#" class="accordion-button collapsed" data-bs-toggle="collapse"
-                                    data-bs-target="#flush-collapseThreer" aria-expanded="false"
-                                    aria-controls="flush-collapseThreer"><i class="fas fa-tv"></i> electronics</a>
-                                <div id="flush-collapseThreer" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <ul>
-                                            <li><a href="#">Consumer Electronic</a></li>
-                                            <li><a href="#">Accessories & Parts</a></li>
-                                            <li><a href="#">other brands</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                            <li><a href="#" class="accordion-button collapsed" data-bs-toggle="collapse"
-                                    data-bs-target="#flush-collapseThreerrp" aria-expanded="false"
-                                    aria-controls="flush-collapseThreerrp"><i class="fas fa-chair-office"></i>
-                                    furnicture</a>
-                                <div id="flush-collapseThreerrp" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <ul>
-                                            <li><a href="#">home</a></li>
-                                            <li><a href="#">office</a></li>
-                                            <li><a href="#">restaurent</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                            <li><a href="#" class="accordion-button collapsed" data-bs-toggle="collapse"
-                                    data-bs-target="#flush-collapseThreerrw" aria-expanded="false"
-                                    aria-controls="flush-collapseThreerrw"><i class="fal fa-mobile"></i> Smart
-                                    Phones</a>
-                                <div id="flush-collapseThreerrw" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <ul>
-                                            <li><a href="#">apple</a></li>
-                                            <li><a href="#">xiaomi</a></li>
-                                            <li><a href="#">oppo</a></li>
-                                            <li><a href="#">samsung</a></li>
-                                            <li><a href="#">vivo</a></li>
-                                            <li><a href="#">others</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </li>
-                            <li><a href="#"><i class="fas fa-home-lg-alt"></i> Home & Garden</a></li>
-                            <li><a href="#"><i class="far fa-camera"></i> Accessories</a></li>
-                            <li><a href="#"><i class="fas fa-heartbeat"></i> healthy & Beauty</a></li>
-                            <li><a href="#"><i class="fal fa-gift-card"></i> Gift Ideas</a></li>
-                            <li><a href="#"><i class="fal fa-gamepad-alt"></i> Toy & Games</a></li>
+                            @endforeach
                             <li><a href="#"><i class="fal fa-gem"></i> View All Categories</a></li>
                         </ul>
                     </div>
