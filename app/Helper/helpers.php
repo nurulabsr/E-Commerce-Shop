144ef3cb1376bae14ee3b95a498eb229a97c022c
nurulabsr <absar.ru@yandex.com>
2024-02-01 18:19:03 +0600

    helper function for active bootstrap property

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -1,1 +1,11 @@
 <?php 
+function SetActive(array $route){
+  
+    if(is_array($route)){
+        foreach($route as $r){
+            if(route()->routeIs($r)){
+             return "active";
+            }
+        }
+    }
+}
