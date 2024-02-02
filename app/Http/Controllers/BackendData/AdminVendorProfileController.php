2f2ad84793dcf03f06f60d967058272ddcffc8ec
nurulabsr <absar.ru@yandex.com>
2024-02-02 15:59:32 +0600

    addded AdminVendorProfileController in BackendData and Vendor model with vendors table

--- app/Http/Controllers/BackendData/AdminVendorProfileController.php
+++ app/Http/Controllers/BackendData/AdminVendorProfileController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\BackendData;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class VendorProfileController extends Controller
+class AdminVendorProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
