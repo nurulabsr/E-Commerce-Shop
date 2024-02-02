e08dd2de2cecf633be238d4cab6a64c26405c964
nurulabsr <absar.ru@yandex.com>
2024-02-02 22:03:57 +0600

    products table attribute added

--- database/migrations/2024_02_02_154753_create_products_table.php
+++ database/migrations/2024_02_02_154753_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->text('product_thumnail_img');
+            $table->string('product_name');
+            $table->string('product_slug');
             $table->timestamps();
         });
     }
