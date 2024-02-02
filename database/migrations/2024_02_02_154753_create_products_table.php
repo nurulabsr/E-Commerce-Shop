4757a36d5e4df9ae69d5e9a8771ccae66e3ea9c7
nurulabsr <absar.ru@yandex.com>
2024-02-02 23:45:02 +0600

    products table attribute added

--- database/migrations/2024_02_02_154753_create_products_table.php
+++ database/migrations/2024_02_02_154753_create_products_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->text('product_thumnail_img');
+            $table->text('product_thumnail_img')->nullable();
             $table->string('product_name');
-            $table->string('product_slug');
-            $table->integer('product_quantity');
-            $table->double('product_price');
+            $table->string('product_slug')->nullable();
+            $table->integer('product_quantity')->nullable();
+            $table->double('product_price')->nullable();
             $table->double('product_offer_price');
             $table->date('product_offer_start_date');
             $table->date('product_offer_end_date');
@@ -28,6 +28,7 @@ return new class extends Migration
             $table->boolean('is_product_top');
             $table->boolean('is_product_best');
             $table->boolean('is_product_featured');
+            $table->integer('is_product_approved');
             $table->boolean('product_status');
             $table->text('product_SEO_title');
             $table->text('product_SEO_description');
