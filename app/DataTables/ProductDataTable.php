be2c55ee339f9f3967b297c89aab906c040d2ba8
nurulabsr <absar.ru@yandex.com>
2024-02-04 20:42:18 +0600

    Yajra Data Table updated

--- app/DataTables/ProductDataTable.php
+++ app/DataTables/ProductDataTable.php
@@ -44,7 +44,7 @@ class ProductDataTable extends DataTable
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
-                    ->orderBy(1)
+                    ->orderBy(0)
                     ->selectStyleSingle()
                     ->buttons([
                         Button::make('excel'),
@@ -62,15 +62,17 @@ class ProductDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            Column::make('id'),
+            Column::make('product_name'),
+            Column::make('product_thumnail_img'),
+            Column::make('product_quantity'),
+            Column::make('product_price'),
+            Column::make('product_short_description'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
                   ->width(60)
                   ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
         ];
     }
 
