ea880760402ef818b0788e83465405d83ce42d1c
nurulabsr <absar.ru@yandex.com>
2024-02-05 23:48:41 +0600

    ProductVariantItemController updated

--- app/Http/Controllers/BackendData/ProductVariantItemController.php
+++ app/Http/Controllers/BackendData/ProductVariantItemController.php
@@ -34,7 +34,9 @@ class ProductVariantItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+
+        ]);
     }
 
     /**
