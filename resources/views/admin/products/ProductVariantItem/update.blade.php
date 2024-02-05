49ba6621f5942a7f4cf8df183cec48fa2d197a48
nurulabsr <absar.ru@yandex.com>
2024-02-05 22:19:17 +0600

    update.blade.php of ProductVariantItem updated

---
+++ resources/views/admin/products/ProductVariantItem/update.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.master')
+@section('content')
+<section class="section">
+    <div class="section-header">
+      <h1>Update Product Variant Item</h1>
+    </div>
+      <div class="section-body">
+      <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                     <h4>Product Variant Item</h4>
+                    <div class="card-header-action">
+                       
+                    </div>
+                </div>
+                <div class="card-body">
+                  <section>
+                    <form action="" method="POST" enctype="multipart/form-data"> 
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                           <label for="">Product Variant Name</label>
+                           <input type="text" name="product_variant_name" class="form-control">
+                        </div>
+                        
+                      
+                        <div class="fom-group">
+                           <label for="">Product Variant Status</label>
+                           <select name="" value=""  class="form-control">
+                               <option value="">Select</option>
+                               <option value="1">Active</option>
+                               <option value="0">In Active</option>
+                           </select>
+                       </div>
+                        <div class="form-group">
+                           <button class="btn btn-primary mt-5">Submit</button>
+                        </div>
+                      </form>
+                  </section>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
