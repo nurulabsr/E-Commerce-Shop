6cbbd97215f0101082e10cc51d8e704b6730195e
nurulabsr <absar.ru@yandex.com>
2024-02-07 20:28:39 +0600

    Shop Profile index.blade.php updated

---
+++ resources/views/vendor/shop-profile/index.blade.php
@@ -0,0 +1,29 @@
+@extends('vendor.Layouts.master')
+@section('dashboard-content')
+      <!--=============================
+    DASHBOARD START
+  ==============================-->
+  <section id="wsus__dashboard">
+    <div class="container-fluid">
+      
+        @include('vendor.Layouts.sidebar')
+
+      <div class="row">
+        <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
+          <div class="dashboard_content mt-2 mt-md-0">
+            <h3><i class="far fa-user"></i> profile</h3>
+            <div class="wsus__dashboard_profile">
+              <div class="wsus__dash_pro_area">
+                <h4>basic information</h4>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--=============================
+    DASHBOARD END
+  ==============================-->
+@endsection
