473a39d57ec2766d0b8ab738dbf6b477924a1367
nurulabsr <absar.ru@yandex.com>
2024-02-07 21:29:28 +0600

    store() of vendor profile

--- app/Http/Controllers/BackendData/VendorShopProfileController.php
+++ app/Http/Controllers/BackendData/VendorShopProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\BackendData;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Http\Request;
 
 class VendorShopProfileController extends Controller
@@ -30,17 +31,29 @@ class VendorShopProfileController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'vendor_profile_banner' =>      ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_phone'  =>      ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_email'  =>      ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_address'=>      ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_description' => ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_facebook_url'=> ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_twitter_url' => ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_insagram_url'=> ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_user_id' =>     ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
-            'vendor_profile_status'  =>     ['required', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_banner' =>      ['required', 'image', 'mimes:png,jpg', 'max:4096', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_phone'  =>      ['required', 'string', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_email'  =>      ['required',  'email' , 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_address'=>      ['required', 'string',  'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_description' => ['required', 'string', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_facebook_url'=> ['required',  'url', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_twitter_url' => ['required',  'url', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_insagram_url'=> ['required',  'url', 'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_user_id' =>     ['required',  'boolean',  'not_regex:/<[^>]*>|[=\';"]/'],
+            'vendor_profile_status'  =>     ['required',  'boolean',  'not_regex:/<[^>]*>|[=\';"]/'],
         ]);
+
+        $vendor = new Vendor();
+        $vendor->admin_vendor_profile_banner         = $request->  ; 
+        $vendor->admin_vendor_profile_phone          = $request->  ;
+        $vendor->admin_vendor_profile_email          = $request->  ;
+        $vendor->admin_vendor_profile_address        = $request->  ;
+        $vendor->admin_vendor_profile_description    = $request->  ;
+        $vendor->admin_vendor_profile_facebook_url   = $request->  ;
+        $vendor->admin_vendor_profile_twitter_url    = $request->  ;
+        $vendor->admin_vendor_profile_insagram_url   = $request->  ;
+        $vendor->admin_vendor_profile_status         = $request->  ;
+        $vendor->admin_vendor_profile_user_id        = $request->  ; 
     }
 
     /**
