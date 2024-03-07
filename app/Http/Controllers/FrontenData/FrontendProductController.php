7fe8c1bc136bfc7bc5c8d98162d4df6e82ca8f06
nurulabsr <absar.ru@yandex.com>
2024-03-07 20:48:22 +0600

    updated

---
+++ app/Http/Controllers/FrontenData/FrontendProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\FrontenData;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FrontendProductController extends Controller
+{
+    //
+}
