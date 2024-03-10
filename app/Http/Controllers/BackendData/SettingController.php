4cf6a5f3498902befed9e61c552e95949061a383
nurulabsr <absar.ru@yandex.com>
2024-03-11 01:50:39 +0600

    updated

--- app/Http/Controllers/BackendData/SettingController.php
+++ app/Http/Controllers/BackendData/SettingController.php
@@ -11,8 +11,17 @@ class SettingController extends Controller
         return view('admin.settings.index');
     }
 
-    public function generalSettingsCreateORUpdate(){
+    public function generalSettingsCreateORUpdate(Request $request){
+      
+       $request->validate([
+          'site_name' => ['required', 'string', 'max:255',],
+          'layout' => ['required', 'string', 'max:255'],
+          'email' => ['required', 'string', 'max:255'],
+          'currency' => ['required', 'string', 'max:255'],
+          'currency_icon' => ['required', 'string', 'max:255'],
+          'timezone' => ['required', 'string', 'max:255'],
+       ]);
 
-        
+       
     }
 }
